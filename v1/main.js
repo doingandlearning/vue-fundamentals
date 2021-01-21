db0f486fff0f022ae39aaf90a61bd206cdcfd372
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-01-21 18:05:21 +0000

    Some edits.

--- v1/main.js
+++ v1/main.js
@@ -10,7 +10,8 @@ const app = Vue.createApp({
         { id: "bronzeTicket", name: "Bronze", image: "http://www.fillmurray.com/200/200", tickets: 3, description: "This is a bronze level fun." },
         { id: "silverTicket", name: "Silver", image: "http://www.fillmurray.com/200/202", tickets: 10, description: "This is some silver level fun." },
         { id: "goldTicket", name: "Gold", image: "http://www.fillmurray.com/199/199", tickets: 40, description: "Go all in - this is the gold level!" }
-      ]
+      ],
+      test: null
     }
   },
   methods: {
@@ -27,5 +28,8 @@ const app = Vue.createApp({
     soldOut() {
       return this.tickets === 0
     }
+  },
+  mounted() {
+    axios.get("https://google.com").then(res => this.test = response)
   }
 })
