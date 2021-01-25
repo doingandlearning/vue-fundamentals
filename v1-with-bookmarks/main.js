3101430c13351840fd722b1d1a62ebcf5601170a
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-01-25 21:06:03 +0000

    v-for

--- v1-with-bookmarks/main.js
+++ v1-with-bookmarks/main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
   data() {
     return {
       title: "Comedy Events",
-      image: "https://via.placeholder.com/200"
+      image: "https://via.placeholder.com/200",
+      details: ["Funny comics", "Mime artistry", "Satire", "Sketches"],
     }
   }
 });
